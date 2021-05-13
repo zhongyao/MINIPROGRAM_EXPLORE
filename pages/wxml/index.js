dcaf2376267828857f8b746dd110d50133c0d3df
hongri <2249869478@qq.com>
2021-05-13 14:42:53 +0800

    模板template:可以在模块中定义代码片段,然后在不同的地方调用

--- pages/wxml/index.js
+++ pages/wxml/index.js
@@ -4,9 +4,10 @@ Page({
   /**
    * 页面的初始数据
    */
+  //data BEGIN
   data: {
     time: (new Date()).toString(),
-    a : 11,
+    a: 11,
     name: "yao",
     length: 3,
     condition: true,
@@ -16,20 +17,27 @@ Page({
     {
       message: "bar"
     }
-  ],
+    ],
 
-  objectArray:[
-    {id: 5, unique: 'unique_5'},
-    {id: 4, unique: 'unique_4'},
-    {id: 3, unique: 'unique_3'},
-    {id: 2, unique: 'unique_2'},
-    {id: 1, unique: 'unique_1'},
-    {id: 0, unique: 'unique_0'},
-  ],
-  numberArray:[1,2,3,4]
+    objectArray: [
+      { id: 5, unique: 'unique_5' },
+      { id: 4, unique: 'unique_4' },
+      { id: 3, unique: 'unique_3' },
+      { id: 2, unique: 'unique_2' },
+      { id: 1, unique: 'unique_1' },
+      { id: 0, unique: 'unique_0' },
+    ],
+    numberArray: [1, 2, 3, 4],
+
+    item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2016-06-18'
+    }
   },
+  //data END
 
-  switch:function(e) {
+  switch: function (e) {
     const length = this.data.objectArray.length
     for (let index = 0; index < length; ++index) {
       const x = Math.floor(Math.random() * length);
@@ -43,16 +51,16 @@ Page({
     })
   },
 
-  addToFront:function(e) {
+  addToFront: function (e) {
     const length = this.data.objectArray.length
-    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+    this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
     this.setData({
       objectArray: this.data.objectArray
     })
   },
 
-  addNumberToFront: function(e){
-    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+  addNumberToFront: function (e) {
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
     this.setData({
       numberArray: this.data.numberArray
     })
