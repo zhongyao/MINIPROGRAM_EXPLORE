cdda65c282141883c8e4753af37af8419e10ddf5
hongri <2249869478@qq.com>
2021-05-17 17:34:10 +0800

    小程序:onLaunch onShow onHide onError方法及调用时机

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // app.js
 console.log('app.js')
 App({
-  onLaunch() {
+  // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+  onLaunch(options) {
+    console.log("onLaunch:" + JSON.stringify(options))
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -14,6 +16,22 @@ App({
       }
     })
   },
+
+  //当小程序启动，或从后台进入前台显示，会触发 onShow
+  onShow(options) {
+    console.log("onShow:" + JSON.stringify(options))
+  },
+
+  //当小程序从前台进入后台，会触发 onHide
+  onHide() {
+    console.log("onHide")
+  },
+
+  //当小程序发生脚本错误，或者 API 调用失败时，会触发 onError 并带上错误信息
+  onError(msg) {
+    console.log("onError:" + msg)
+  },
+
   globalData: {
     userInfo: null
   }
