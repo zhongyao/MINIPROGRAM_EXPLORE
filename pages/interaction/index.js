612a08b552c3d1ccd0509e86a156dbe47b383d06
hongri <2249869478@qq.com>
2021-07-15 17:30:59 +0800

    下拉刷新 上拉触底

--- pages/interaction/index.js
+++ pages/interaction/index.js
@@ -23,19 +23,19 @@ Page({
     // wx.hideToast();
 
     //展示对话框
-    wx.showModal({
-      title: '标题',
-      content: '告知当前状态，信息和解决方法',
-      confirmText: '主操作',
-      cancelText: '次要操作',
-      success: function(res) {
-        if (res.confirm) {
-          console.log('用户点击主操作')
-        } else if (res.cancel) {
-          console.log('用户点击次要操作')
-        }
-      }
-    })
+    // wx.showModal({
+    //   title: '标题',
+    //   content: '告知当前状态，信息和解决方法',
+    //   confirmText: '主操作',
+    //   cancelText: '次要操作',
+    //   success: function(res) {
+    //     if (res.confirm) {
+    //       console.log('用户点击主操作')
+    //     } else if (res.cancel) {
+    //       console.log('用户点击次要操作')
+    //     }
+    //   }
+    // })
   },
 
   /**
@@ -70,14 +70,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('下拉刷新')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('上拉触底')
   },
 
   /**
